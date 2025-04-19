785d23b6d0cb2ca74250d8d01a6588bdc785857d
Vishal <86146413+BrainsCorp@users.noreply.github.com>
2025-04-19 02:53:27 -0400

    add data loading script dataset.py

---
+++ predicting_cardio_disease/cardiovascular_disease_prediction/config.py
@@ -0,0 +1,16 @@
+# This file contains the configuration for the cardiovascular disease prediction project.
+
+# Kaggle Link to the dataset
+DATASET_LINK = "https://www.kaggle.com/api/v1/datasets/download/sulianova/cardiovascular-disease-dataset"
+
+# raw zip file name
+DATASET_ZIPFILE_NAME = "cardio_disease.zip"
+
+# dataset name
+DATASET_NAME = "cardio_train.csv"
+
+# Directory where the dataset will be stored
+DATASET_DIR = "data\\raw"
+
+# Project Directory
+PROJECT_DIR = "predicting_cardio_disease"
